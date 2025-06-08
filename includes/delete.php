b4cd338fb935c652c896f5407a3d34257f00e632
lupus6D616E <192499452+Lupus6D616E@users.noreply.github.com>
2025-06-08 08:04:39 +0800

    added delete function but kinda scuffed

---
+++ includes/delete.php
@@ -0,0 +1,38 @@
+<?php
+include 'db_connect.php';
+session_start();
+
+// Check if the user is logged in, if
+// not then redirect them to the login page
+if (!isset($_SESSION['email'])) {
+    header("Location: login.php");
+    exit();
+}
+$offset = $_GET['offset'];
+$id = $_GET['id'];
+header("Refresh: 1; URL=../index.php"); 
+    $getOtherStmt = $conn->prepare("SELECT emailID FROM workers WHERE workerId = ?");
+    $getOtherStmt->bind_param("i", $id,);
+    $getOtherStmt->execute();
+    $getOtherStmt->store_result();
+    $getOtherStmt->bind_result($emailid);
+    $getOtherStmt->fetch();
+    
+    $deleteLeaveStmt = $conn->prepare("DELETE FROM leaves WHERE authorid = ?");
+    $deleteLeaveStmt->bind_param("i", $id);
+    $deleteLeaveStmt->execute();
+    $deleteLeaveStmt->store_result();
+
+    
+    $deleteWorkerInfoStmt = $conn->prepare("DELETE FROM workers WHERE workerId = ?");
+    $deleteWorkerInfoStmt->bind_param("i", $id);
+    $deleteWorkerInfoStmt->execute();
+    $deleteWorkerInfoStmt->store_result();
+
+    $deleteAccountInfoStmt = $conn->prepare("DELETE FROM users WHERE userid = ?");
+    $deleteAccountInfoStmt->bind_param("i", $emailid);
+    $deleteAccountInfoStmt->execute();
+    $deleteAccountInfoStmt->store_result();
+
+?>
+just deleted <? echo $id ?>
